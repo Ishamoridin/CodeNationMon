210501c66d1bcc9f1b288e9c7a861bf908d6370e
Ishamoridin <Ishamoridin@googlemail.com>
2022-11-16 11:42:10 +0000

    creature class and methods (first draft)

--- backend.js
+++ backend.js
@@ -1,1 +1,36 @@
-const modules = require 
+const modules = require(`./funBin.js`);
+const gameText = document.getElementById(`gameText`)
+class creature {
+    constructor(name, species) {
+        this.name=name;
+        this.species=species;
+        this.health=100;
+        this.satiation=100;
+        this.happiness=100;
+        this.energy=100;
+    }
+    eats(){
+        if (this.satiation<=50){gameText.innerText=`${this.name} digs in to a hearty meal`
+            this.satiation+=50}
+            else {gameText.innerText=`${this.name} is too full to eat right now`}
+    }
+    rests(){
+        if (this.energy<=50){gameText.innerText=`${this.name} begins to rest`
+            this.energy+=50}
+            else {gameText.innerText=`${this.name} isn't tired enough to rest`}
+    }
+    plays(){
+        if (this.happiness<=50 && this.health>=50 && this.satiation>=50 && this.energy>=50){
+            gameText.innerText=`${this.name} is happy that you played with them`
+            this.happiness+=50}
+        else if (this.health<50){
+            gameText.innerText=`${this.name} is too unwell to play right now`
+        }
+        else if (this.satiation<50){
+            gameText.innerText=`${this.name} is too hungry to play right now`
+        }
+        else if (this.energy<50){
+            gameText.innerText=`${this.name} is too tired to play right now`
+        }
+    }
+}
